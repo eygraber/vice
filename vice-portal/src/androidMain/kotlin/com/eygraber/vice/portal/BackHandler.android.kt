355418dde27cb2bf3371744edc87bec8ac82ea60
Eliezer Graber <me@eygraber.com>
2024-01-15 16:10:58 -0500

    Remove BackHandler from VICE (#8)

--- vice-portal/src/androidMain/kotlin/com/eygraber/vice/portal/BackHandler.android.kt
+++
@@ -1,8 +0,0 @@
-package com.eygraber.vice.portal
-
-import androidx.compose.runtime.Composable
-
-@Composable
-public actual fun BackHandler(enabled: Boolean, onBack: () -> Unit) {
-  androidx.activity.compose.BackHandler(enabled, onBack)
-}
