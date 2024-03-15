e819583c5b11d21bdd116a2d7a81096d1f6a2905
Eliezer Graber <me@eygraber.com>
2024-03-14 23:58:02 -0400

    Update PR checks

--- vice-core/src/iosMain/kotlin/com/eygraber/vice/EpochMillis.ios.kt
+++ vice-core/src/iosMain/kotlin/com/eygraber/vice/EpochMillis.ios.kt
@@ -1,5 +1,6 @@
 package com.eygraber.vice
 
 import platform.Foundation.NSDate
+import platform.Foundation.timeIntervalSince1970
 
 internal actual fun epochMillis(): Long = (NSDate().timeIntervalSince1970 * 1_000).toLong()
