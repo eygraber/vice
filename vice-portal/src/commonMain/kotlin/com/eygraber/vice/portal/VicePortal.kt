624b4730d8a0116860b43f7e96bddb07fa4cad3e
Eliezer Graber <me@eygraber.com>
2024-03-19 13:45:49 -0400

    Implement intents in VicePortal (#60)

--- vice-portal/src/commonMain/kotlin/com/eygraber/vice/portal/VicePortal.kt
+++ vice-portal/src/commonMain/kotlin/com/eygraber/vice/portal/VicePortal.kt
@@ -6,9 +6,13 @@ import com.eygraber.vice.ViceCompositor
 import com.eygraber.vice.ViceContainer
 import com.eygraber.vice.ViceEffects
 import com.eygraber.vice.ViceView
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
 
 public abstract class VicePortal<K, V, I, C, E, S> : ComposePortal<K>, ViceContainer<V, I, C, E, S>
   where V : ViceView<I, S>, C : ViceCompositor<I, S>, E : ViceEffects {
+  override val intents: SharedFlow<I> = MutableSharedFlow(extraBufferCapacity = 64)
+
   @Composable
   public final override fun Render() {
     Vice()
