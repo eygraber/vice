a9edbccb3b69bf2817a9b69d63acb76af8f41fef
Eliezer Graber <me@eygraber.com>
2024-03-21 16:05:36 -0400

    Remove the intent param from Compositor.composite

--- vice-core/src/commonMain/kotlin/com/eygraber/vice/ViceCompositor.kt
+++ vice-core/src/commonMain/kotlin/com/eygraber/vice/ViceCompositor.kt
@@ -1,14 +1,10 @@
 package com.eygraber.vice
 
 import androidx.compose.runtime.Composable
-import kotlinx.coroutines.flow.Flow
 
 public abstract class ViceCompositor<Intent, State> {
   @Composable
-  internal fun internalComposite(intents: Flow<Intent>): State = composite(intents)
-
-  @Composable
-  protected abstract fun composite(intents: Flow<Intent>): State
+  public abstract fun composite(): State
 
   internal suspend fun internalOnIntent(intent: Intent) = onIntent(intent)
   protected open suspend fun onIntent(intent: Intent) {}
