5fc17e896f7ba80cb88b75a8e0ff08f9ad44fdbe
Eliezer Graber <me@eygraber.com>
2024-05-26 18:53:59 -0400

    StateFlowSource isn't a State because value and currentState aren't synced

--- vice-sources/src/commonMain/kotlin/com/eygraber/vice/sources/StateFlowSource.kt
+++ vice-sources/src/commonMain/kotlin/com/eygraber/vice/sources/StateFlowSource.kt
@@ -8,12 +8,12 @@ import com.eygraber.vice.ViceSource
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.StateFlow
 
-public abstract class StateFlowSource<T> : ViceSource<T>, State<T> {
+public abstract class StateFlowSource<T> : ViceSource<T> {
   protected abstract val flow: StateFlow<T>
 
   protected abstract suspend fun onAttached(scope: CoroutineScope)
 
-  override val value: T get() = flow.value
+  public val value: T get() = flow.value
 
   @Composable
   final override fun currentState(): T {
