4b21a845dd5d092aaaf3dc6108b37c4a4c08f5c8
Eliezer Graber <me@eygraber.com>
2024-08-09 00:53:28 -0400

    Manually add HiddenFromObjC annotations
      - https://youtrack.jetbrains.com/issue/CMP-5908

--- samples/nav/shared/src/commonMain/kotlin/com/eygraber/vice/samples/nav/shared/home/HomeDestination.kt
+++ samples/nav/shared/src/commonMain/kotlin/com/eygraber/vice/samples/nav/shared/home/HomeDestination.kt
@@ -1,7 +1,11 @@
 package com.eygraber.vice.samples.nav.shared.home
 
 import com.eygraber.vice.samples.nav.shared.SampleDestination
+import kotlin.experimental.ExperimentalObjCRefinement
+import kotlin.native.HiddenFromObjC
 
+@OptIn(ExperimentalObjCRefinement::class)
+@HiddenFromObjC
 class HomeDestination(
   onNavigateToCreateItem: () -> Unit,
   onNavigateToUpdateItem: (String) -> Unit,
--- samples/nav/shared/src/commonMain/kotlin/com/eygraber/vice/samples/nav/shared/settings/SettingsDestination.kt
+++ samples/nav/shared/src/commonMain/kotlin/com/eygraber/vice/samples/nav/shared/settings/SettingsDestination.kt
@@ -1,7 +1,11 @@
 package com.eygraber.vice.samples.nav.shared.settings
 
 import com.eygraber.vice.samples.nav.shared.SampleDestination
+import kotlin.experimental.ExperimentalObjCRefinement
+import kotlin.native.HiddenFromObjC
 
+@OptIn(ExperimentalObjCRefinement::class)
+@HiddenFromObjC
 class SettingsDestination(
   onNavigateBack: () -> Unit,
   onNavigateToAboutUs: () -> Unit,
