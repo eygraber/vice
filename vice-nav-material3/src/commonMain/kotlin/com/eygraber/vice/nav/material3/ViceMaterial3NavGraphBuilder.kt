47cf0896fe0bf7002db077d90ca932a77960b19c
Eliezer Graber <me@eygraber.com>
2025-05-15 19:45:56 -0400

    Add more bottomSheet params to viceBottomSheet

--- vice-nav-material3/src/commonMain/kotlin/com/eygraber/vice/nav/material3/ViceMaterial3NavGraphBuilder.kt
+++ vice-nav-material3/src/commonMain/kotlin/com/eygraber/vice/nav/material3/ViceMaterial3NavGraphBuilder.kt
@@ -1,16 +1,24 @@
 package com.eygraber.vice.nav.material3
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheetDefaults
 import androidx.compose.material3.ModalBottomSheetProperties
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.SheetValue
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavDeepLink
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.toRoute
+import com.eygraber.compose.material3.navigation.ModalBottomSheetNavigator
 import com.eygraber.compose.material3.navigation.bottomSheet
+import com.eygraber.compose.material3.navigation.defaultWindowInsets
 import com.eygraber.vice.nav.TypedNavBackStackEntry
 import com.eygraber.vice.nav.ViceDestination
 import kotlin.jvm.JvmSuppressWildcards
@@ -21,16 +29,24 @@ public fun NavGraphBuilder.viceBottomSheet(
   route: String,
   arguments: List<NamedNavArgument> = emptyList(),
   deepLinks: List<NavDeepLink> = emptyList(),
+  modalBottomSheetModifier: Modifier = Modifier,
   modalBottomSheetProperties: ModalBottomSheetProperties = ModalBottomSheetDefaults.properties,
   skipPartiallyExpanded: Boolean = false,
+  confirmValueChange: (SheetValue) -> Boolean = { true },
+  contentWindowInsets: @Composable (SheetState) -> WindowInsets = ModalBottomSheetNavigator.defaultWindowInsets(),
+  dragHandle: @Composable (() -> Unit)? = { BottomSheetDefaults.DragHandle() },
   destinationFactory: (NavBackStackEntry) -> ViceDestination<*, *, *, *>,
 ) {
   bottomSheet(
     route = route,
     arguments = arguments,
     deepLinks = deepLinks,
+    modalBottomSheetModifier = modalBottomSheetModifier,
     modalBottomSheetProperties = modalBottomSheetProperties,
     skipPartiallyExpanded = skipPartiallyExpanded,
+    confirmValueChange = confirmValueChange,
+    contentWindowInsets = contentWindowInsets,
+    dragHandle = dragHandle,
   ) {
     remember(it.id) { destinationFactory(it) }.Vice()
   }
@@ -40,15 +56,24 @@ public fun NavGraphBuilder.viceBottomSheet(
 public inline fun <reified T : Any> NavGraphBuilder.viceBottomSheet(
   typeMap: Map<KType, @JvmSuppressWildcards NavType<*>> = emptyMap(),
   deepLinks: List<NavDeepLink> = emptyList(),
+  modalBottomSheetModifier: Modifier = Modifier,
   modalBottomSheetProperties: ModalBottomSheetProperties = ModalBottomSheetDefaults.properties,
   skipPartiallyExpanded: Boolean = false,
+  noinline confirmValueChange: (SheetValue) -> Boolean = { true },
+  noinline contentWindowInsets: @Composable (SheetState) -> WindowInsets =
+    ModalBottomSheetNavigator.defaultWindowInsets(),
+  noinline dragHandle: @Composable (() -> Unit)? = { BottomSheetDefaults.DragHandle() },
   crossinline destinationFactory: (TypedNavBackStackEntry<T>) -> ViceDestination<*, *, *, *>,
 ) {
   bottomSheet<T>(
     typeMap = typeMap,
     deepLinks = deepLinks,
+    modalBottomSheetModifier = modalBottomSheetModifier,
     modalBottomSheetProperties = modalBottomSheetProperties,
     skipPartiallyExpanded = skipPartiallyExpanded,
+    confirmValueChange = confirmValueChange,
+    contentWindowInsets = contentWindowInsets,
+    dragHandle = dragHandle,
   ) {
     remember(it.id) { destinationFactory(TypedNavBackStackEntry(it.toRoute<T>(), it)) }.Vice()
   }
