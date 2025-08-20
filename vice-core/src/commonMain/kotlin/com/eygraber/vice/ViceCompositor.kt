c00241a6e30beb2b08b38783f61d0375f8927a18
Eliezer Graber <me@eygraber.com>
2025-08-20 14:36:46 -0400

    Add some helpful comments

--- vice-core/src/commonMain/kotlin/com/eygraber/vice/ViceCompositor.kt
+++ vice-core/src/commonMain/kotlin/com/eygraber/vice/ViceCompositor.kt
@@ -2,6 +2,9 @@ package com.eygraber.vice
 
 import androidx.compose.runtime.Composable
 
+/**
+ * It is inadvisable to override [equals] and [hashCode] for a [ViceCompositor].
+ */
 public interface ViceCompositor<Intent, State> {
   @Composable
   public fun composite(): State
